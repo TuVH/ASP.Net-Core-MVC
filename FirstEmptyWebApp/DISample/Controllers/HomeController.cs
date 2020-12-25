af7e62a154669b2498e50b7cf3fa8ae1b5657724
Tu <hoangtu1392001@gmail.com>
2020-12-26 00:30:40 +0700

    [Add]_Entities

---
+++ FirstEmptyWebApp/DISample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using DISample.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DISample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ProductService _productService;
+
+        HomeController()
+        {
+            _productService = new ProductService();
+        }
+        public IActionResult Index()
+        {
+            var products = _productService.GetAll();
+            return View(products);
+        }
+    }
+}
---
+++ FirstEmptyWebApp/StaticFiles/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StaticFiles.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Vip()
+        {
+            return View();
+        }
+    }
+}
