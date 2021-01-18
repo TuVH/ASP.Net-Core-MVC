30303b40f8914fadac89309f33c4374128584014
Tu <hoangtu1392001@gmail.com>
2021-01-18 11:34:48 +0700

    [Add]_Data seeding of enity framework

--- FirstEmptyWebApp/PointsLookup/Data/ApplicationDbContext.cs
+++ FirstEmptyWebApp/PointsLookup/Data/ApplicationDbContext.cs
@@ -16,5 +16,49 @@ namespace PointsLookup.Data
 
         public DbSet<Student> Students { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    Id=1,
+                    IdentNumber ="732001",
+                    Name="Nguyễn Quốc Hải",
+                    DateOfBirth= new DateTime(2001,09,13),
+                    MathScores=9,
+                    LiteratureScores=7,
+                    EnglishScores =7.3,
+                    BiologicalScores=8,
+                    PhysicalScores = 6.3,
+                    ChemiscalScores = 4
+                },
+                new Student
+                {
+                    Id=2,
+                    IdentNumber = "732002",
+                    Name = "Lê Thanh Nhàn",
+                    DateOfBirth = new DateTime(2001, 05, 05),
+                    MathScores = 8,
+                    LiteratureScores = 6,
+                    EnglishScores = 9,
+                    BiologicalScores = 4,
+                    PhysicalScores = 5.5,
+                    ChemiscalScores = 6
+                },
+                new Student
+                {
+                    Id=3,
+                    IdentNumber = "732003",
+                    Name = "Vũ Hoàng Tú",
+                    DateOfBirth = new DateTime(2001, 05, 05),
+                    MathScores = 8,
+                    LiteratureScores = 6,
+                    EnglishScores = 9,
+                    BiologicalScores = 4,
+                    PhysicalScores = 5.5,
+                    ChemiscalScores = 6
+                }
+            );
+        }
     }
 }
---
+++ FirstEmptyWebApp/PointsLookup/Migrations/20210118042955_SeedStudentsToTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PointsLookup.Migrations
+{
+    public partial class SeedStudentsToTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "BiologicalScores", "ChemiscalScores", "DateOfBirth", "EnglishScores", "IdentNumber", "LiteratureScores", "MathScores", "Name", "PhysicalScores" },
+                values: new object[] { 1, 8.0, 4.0, new DateTime(2001, 9, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), 7.2999999999999998, "732001", 7.0, 9.0, "Nguyễn Quốc Hải", 6.2999999999999998 });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "BiologicalScores", "ChemiscalScores", "DateOfBirth", "EnglishScores", "IdentNumber", "LiteratureScores", "MathScores", "Name", "PhysicalScores" },
+                values: new object[] { 2, 4.0, 6.0, new DateTime(2001, 5, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 9.0, "732002", 6.0, 8.0, "Lê Thanh Nhàn", 5.5 });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "BiologicalScores", "ChemiscalScores", "DateOfBirth", "EnglishScores", "IdentNumber", "LiteratureScores", "MathScores", "Name", "PhysicalScores" },
+                values: new object[] { 3, 4.0, 6.0, new DateTime(2001, 5, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 9.0, "732003", 6.0, 8.0, "Vũ Hoàng Tú", 5.5 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
